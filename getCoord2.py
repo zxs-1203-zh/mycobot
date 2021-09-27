792a56f1b4d4b0f7a4b58e1e8c7d2d55042c4cd8
zxs-1203-zh <15021193371@163com>
2021-09-27 08:51:54 +0600

    getCoord

--- getCoord2.py
+++ getCoord2.py
@@ -5,7 +5,7 @@ import threading
 import time
 
 mc = MyCobot(port.port)
-threadMov = threading.Thread(target = move.mov1, args = (mc,))
+threadMov = threading.Thread(target = move.mov2, args = (mc,))
 threadMov.start()
 while move.isMoving:
     print(mc.get_coords(), time.time())
--- move.py
+++ move.py
@@ -3,9 +3,13 @@ from pymycobot.genre import Angle, Coord
 import init
 import time
 
+isMoving = True
+
 def mov1(mc : MyCobot):
+    global isMoving
+    isMoving = True
     init.init(mc)
-    time.sleep(10)
+    time.sleep(7)
     mc.send_angle(Angle.J1.value, 90, 30)
     time.sleep(5)
     mc.send_angle(Angle.J1.value, -90, 30)
@@ -41,11 +45,24 @@ def mov1(mc : MyCobot):
     mc.send_angle(Angle.J6.value, -90, 30)
     time.sleep(5)
     mc.send_angle(Angle.J6.value, 0, 30)
-    time.sleep(5)
+    isMoving = False
 
 def mov2(mc : MyCobot):
+    global isMoving
+    isMoving = True
     init.init(mc)
+    time.sleep(7)
+    mc.send_angle(Angle.J3.value, 45, 30)
+    for index in range(10):
+        time.sleep(5)
+        mc.send_angle(Angle.J1.value, -150, 30)
+        time.sleep(5)
+        mc.send_angle(Angle.J1.value, 150, 30)
+    isMoving = False
 
 def mov3(mc : MyCobot):
+    global isMoving
+    isMoving = True
     init.init(mc)
+    isMoving = False
 
